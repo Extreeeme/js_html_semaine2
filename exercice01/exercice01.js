d734a0d25a94b1e24df0f69100a97fc4f70c8d0e
Martin Dagostino <martindcontact@gmail.com>
2016-12-10 17:58:10 +0100

    exercice n°01.7 fini

--- exercice01/exercice01.js
+++ exercice01/exercice01.js
@@ -1,4 +1,15 @@
-var prenom = prompt("Quel est votre prenom ?");
-var nom = prompt("Quel est votre nom ?");
-var ville = prompt("Où habitez vous ?");
-alert("Nom : " + nom + "\n Prénom : " + prenom + "\n Ville : " + ville);
+var age = prompt("Quel âge avez-vous ?");
+var sexe = prompt("Êtes vous un garçon ou une fille ?");
+var nationalite = prompt("De quelle nationalité êtes-vous ?");
+if (age > 18 && sexe == "garçon") {
+	alert("Vous êtes un garçon majeur " +nationalite);
+}
+else if (age > 18 && sexe == "fille"){
+	alert("Vous êtes une fille majeur " +nationalite);
+}
+else if (age <= 18 && sexe == "fille"){
+	alert("Vous êtes une fille mineur " +nationalite);
+}
+else if (age <= 18 && sexe == "garçon") {
+	alert("Vous êtes un garçon mineur " +nationalite);
+}
