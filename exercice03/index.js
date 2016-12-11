eb5fcf802d88a85d4769d7de14754246e52d24a3
Extreeeme <martindcontact@gmail.com>
2016-12-11 22:13:43 +0100

    exercice n°03.1 fini

---
+++ exercice03/index.js
@@ -0,0 +1,10 @@
+function masquerTout(id) { 
+    var cible = document.getElementById("texte"); 
+    cible.style.display = 'none' ; 
+    
+} 
+
+function visibilite(id) { 
+    var cible = document.getElementById("texte"); 
+    cible.style.display = 'block'; 
+}
