d78166d0542745fae9303471b021688c8c226982
Extreeeme <martindcontact@gmail.com>
2016-12-11 15:31:20 +0100

    exercice n°02.4 fini

--- exercice02/index.js
+++
@@ -1,6 +0,0 @@
-function resetFields(){
-document.getElementById("nom").value="";
-document.getElementById("prenom").value=""
-document.getElementById("ville").value=""
-document.getElementById('select1').selectedIndex=-1;
-}
