e24d3a50949906831160d2e25cf6417ba98e1136
Martin D <martindcontact@gmail.com>
2016-12-12 14:45:28 +0100

    Corrigé exercice n°01.4

--- exercice01/exercice01.js
+++ exercice01/exercice01.js
@@ -1,4 +1,15 @@
 var nombre1 = prompt("Entrez un nombre à virgule : ");
+if(!isNan(nombre1)){
+  nombre1 = Math.round(nombre1);
+}
+else{
+  alert("erreur \n Merci de mettre un . et non une ,");
+}
 var nombre2 = prompt("Entrez un deuxième nombre à virgule :");
-var resultat = parseInt(nombre1)*nombre2;
-alert(resultat);
+if (isNan(nombre2)){
+  alert("erreur \n Merci de mettre un . et non une ,");
+}
+else {
+  var resultat = parseInt(nombre1)*nombre2;
+  alert(resultat);
+}
